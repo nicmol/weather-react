dcabcb179eb008ac9268604613572942700b667f
Nicolette <molitorn@my.lanecc.edu>
2019-06-08 17:35:51 -0700

    changes were not committed

---
+++ src/components/WeatherListItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class WeatherListItem extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    this.onClick = this.onClick.bind(this);
+  }
+  
+  onClick () {
+    const { onDayClicked, index } = this.props;
+    onDayClicked(index);
+}
+
+  render() {
+    const { forecastDay } = this.props;
+    const date = new Date(forecastDay.dt * 1000);
+    const weekday = this.getWeekday(date);
+    return (
+        <div class="weather-list-item" onClick={this.onClick}>
+            <h2>{date.getMonth() + 1} / {date.getDate()}</h2>
+            <h3>{weekday}</h3>
+            <h3>{forecastDay.minTemp.toFixed(1)}&deg;F &#124; {forecastDay.maxTemp.toFixed(1)}&deg;F</h3>
+        </div>
+    );
+  }
+
+  getWeekday(date) {
+    const dayNames = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const weekday = date.getDay();
+    return dayNames[weekday];
+  }
+}
+export default WeatherListItem;
